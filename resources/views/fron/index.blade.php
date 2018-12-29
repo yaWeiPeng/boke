fa70e9f5da816801f7172678e61ee1e9715cdddf
keweipeng <337018308@qq.com>
2018-12-29 15:24:16 +0800

    模板继承

--- resources/views/fron/index.blade.php
+++ resources/views/fron/index.blade.php
@@ -1,24 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title></title>
-</head>
+@extends('public')
+@section('title','主页')
+@section('content')
+<link href="/css/member/login.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="/js/jquery-3.3.1.js"></script>
+<script type="text/javascript" src="/js/member/login.js"></script>
 <link href="/css/index.css" type="text/css" rel="stylesheet" />
 <script type="text/javascript" src="/js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="/js/index.js"></script>
-<body>
+
 	 <!-- style="background-image: url('/background/icome.jpg')" -->
-	<div id="top">
-		<div class="icome-center">
-			<div class="icome-border">
-				<img src="/touxiang/moren.jpeg" class="icome">
-				<a href="/login">登录</a>
-				<a href="/reg" style="float: right;">注册</a>
-				<!-- <span>欢迎，</span>
-				<span>用户</span> -->
-			</div>
+<div id="top">
+	<div class="icome-center">
+		<div class="icome-border">
+			<img src="/touxiang/moren.jpeg" class="icome">
+			<a href="/login">登录</a>
+			<a href="/reg" style="float: right;">注册</a>
+			<!-- <span>欢迎，</span>
+			<span>用户</span> -->
 		</div>
 	</div>
-</body>
-</html>
+	<img src="/background/扫码领红包.png" width="400px" height="600px" style="padding-left: 200px"><span style="font-size: 50px">网站尚未完善,扫个红包休息一下</span>
+</div>
+@stop
