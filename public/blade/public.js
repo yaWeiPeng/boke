2f0e549e1e5dd7637a33b9d22ebd5b4653f549ef
keweipeng <337018308@qq.com>
2018-12-29 15:13:40 +0800

    模板继承

---
+++ public/blade/public.js
@@ -0,0 +1,10 @@
+$(".head-two").hover(function(){
+	$(".head-three").show();
+},function(){
+	$(".head-three").hide();
+});
+$(".head-four").hover(function(){
+	$(".head-five").show();
+},function(){
+	$(".head-five").hide();
+});
